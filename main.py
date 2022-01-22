6d17b7564dfb1783e8bd59d49f51d9f67666c1f6
Sean Slater <seanslater@whatno.io>
2022-01-21 21:11:49 -0500

    choose yaml

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello ggj")
+import readline
+import shlex
+import yaml
+from pprint import pprint
+
+with open("test.yml") as fp:
+    config = yaml.load(fp, Loader=yaml.FullLoader)
+
+pprint(config)
+
+
+def get_input(prompt):
+    return shlex.split(input(prompt))
+
+# res = get_input("What is you're name?\n")
+# print(res)
+
+
+def run():
+    while True:
+        print(res := get_input("Question? "))
+        if res[0] == "quit":
+            break
+
+run()
